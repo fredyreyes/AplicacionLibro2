6f8bc24d5efe681ec6d2e626e743c8a6ae6d625d
fredyreyes <fredy.reyes.a@gmail.com>
2017-10-02 23:12:24 -0500

    Modificacion DOS

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fredy.aplicacionlibro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //TE AMO JOHAN2!!
+//cambio 3
 public class MainActivity extends AppCompatActivity {
 
     @Override
