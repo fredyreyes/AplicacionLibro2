868d7bb0b5d79c1d9942f8a5c35fa13159f7cd0c
fredyreyes <fredy.reyes.a@gmail.com>
2017-10-02 22:03:03 -0500

    Modificacion Uno

--- app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
+++ app/src/main/java/com/example/fredy/aplicacionlibro/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.fredy.aplicacionlibro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//TE AMO JOHAN!!
 public class MainActivity extends AppCompatActivity {
 
     @Override
